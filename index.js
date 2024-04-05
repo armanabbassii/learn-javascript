70e59b4c022d1fbee00a30a33965974c1147073e
armanabbassii <arman.abbasi.nt@gmail.com>
2024-04-05 23:03:24 +0330

    check

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.getElementById("count-el").innerText = 5;
+// console.log(myAge);
+// myAge = 34;
+// print(myAge);
+let myAge = 10;
+let humanDogRatio = 7;
+
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
